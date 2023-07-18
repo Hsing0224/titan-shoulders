89313afd5123cbfa9c40c1e54320efb2ade4eead
Hsing Chen <twrandy24@gmail.com>
2023-07-18 13:50:26 +0800

    修改baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://hsing0224.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/titanShoulders/doc/',
+  baseUrl: '/titanShoulders/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
